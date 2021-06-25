1a039b9983fc7ec47040c7d4a6fa0f9014c4d942
Dean Pinlac <dean.pinlac@gmail.com>
2021-06-25 00:17:35 -0400

    Add logic for Delete

--- server/routes/books.js
+++ server/routes/books.js
@@ -103,9 +103,19 @@ router.post('/:id', (req, res, next) => {
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+   let id = req.params.id;
+
+   book.deleteOne({_id: id}, {}, (err) => {
+     if(err){
+       console.error(err);
+       res.end(err);
+     }
+
+     res.redirect('/books');
+   });
 });
 
 
